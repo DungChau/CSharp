be6ea894832ee98d16b903ac2ee43c7fbbc63c14
Dung Chau <viet.lunar@gmail.com>
2013-02-19 19:25:48 -0800

    add Primes and start GUI

--- Prime/Prime/Program.cs
+++ Prime/Prime/Program.cs
@@ -6,10 +6,49 @@ using System.Threading.Tasks;
 
 namespace Prime
 {
+    public class Primes
+    {
+        private int lim;
+        public Primes(): this(50)
+        {
+        }
+
+        public Primes(int limit)
+        {
+            lim = limit;
+        }
+        public List<int> primeGenerate() {
+            List<int> list = new List<int>();
+            bool flag;
+            for (int i = 2; i < lim; i++)
+            {
+                flag = true;
+                for (int j = 2; j < i; j++)
+                {
+                    if ((i % j) == 0)
+                    {
+                        flag = false;
+                        break;
+                    }               
+                }
+                if (flag)
+                {
+                    list.Add(i);
+                } 
+            }
+            return list;
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            Primes primes = new Primes();
+            List<int> l = primes.primeGenerate();
+            foreach(var p in l ){
+                Console.WriteLine(p);
+            }
+            Console.ReadKey();
         }
     }
 }
